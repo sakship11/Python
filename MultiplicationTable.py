c2050dc95ba555563025cab07edafe8902b2f5a6
Sakshi Pawar <88649916+sakship11@users.noreply.github.com>
2024-01-25 20:07:18 +0530

    Multiplication Table

---
+++ MultiplicationTable.py
@@ -0,0 +1,7 @@
+number = int(input ("Enter the number: "))      
+count = 1          
+print ("The Multiplication Table of: ", number)    
+while count <= 10:    
+    number = number * 1    
+    print (number, 'x', i, '=', number * count)    
+    count += 1    
